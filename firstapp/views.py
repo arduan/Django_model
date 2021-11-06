d831f5de709f8cd74426956b23007fc8cafd4380
Vitaly <ivanov.arduan@yandex.ru>
2021-11-06 08:41:14 +0300

    Добавил коментарии

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponseRedirect
 from .models import Person
 
 
+# Получение данных из БД и загрузка index.html
 def index(request):
     people = Person.objects.all()
     return render(request, 'index.html', {'people': people})
 
-
+# Сохранение данных в БД
 def create(request):
     if request.method == 'POST':
         client = Person()
