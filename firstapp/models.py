209cee8fb572f555573841bde4d1213bbe0571cc
Vitaly <ivanov.arduan@yandex.ru>
2021-11-07 07:54:00 +0300

    Создание модели один ко многим.

--- firstapp/models.py
+++ firstapp/models.py
@@ -9,3 +9,13 @@ class Person(models.Model):
     age = models.IntegerField()
     objects = models.Manager()
     # DoesNotExist = models.Manager
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+# Создание модели один ко многим.
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
